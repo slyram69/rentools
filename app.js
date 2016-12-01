aa2c5cbbbdaeff3de64eb68610f517f631ba8c5f
joshmcallen <joshmcallen@gmail.com>
2016-12-01 13:40:07 -0600

    item view created

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var multer = require('multer');
 
 mongoose.connect('mongodb://localhost/rentools');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var newitem = require('./routes/newitem');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(multer({dest: 'public/images/uploads'}).single('image'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/newitem', newitem);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
